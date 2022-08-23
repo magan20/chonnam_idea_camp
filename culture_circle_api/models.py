44c49765760bdfd3d1e548ed0ce105a58a0f5ad0
magan20 <audwls2160@naver.com>
2022-08-23 16:08:14 +0900

    modified api model

--- culture_circle_api/models.py
+++ culture_circle_api/models.py
@@ -3,8 +3,13 @@ from django.db import models
 # Create your models here.
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
 class Show(models.Model):
     title = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     poster_dir_path = models.CharField(max_length=200)
     region = models.CharField(max_length=100)
     start_date = models.DateField(blank=False)
